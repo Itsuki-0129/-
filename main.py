352ba92451caf343d2c774316d88f1672a7ef30b
Itsuki-0129 <nagaoituki@gmail.com>
2020-12-18 03:43:33 +0900

    アップロード情報の閲覧、検索などほぼできたんじゃない？といった内容。あと表のCSSをがんばりました。

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort, json, jsonify, url_for, send_from_directory
+from flask import Flask, session, abort, json, jsonify, send_from_directory
+from flask import Flask, flash, redirect, render_template, request, url_for
 from werkzeug.utils import secure_filename
-import pymysql
-import requests
-import os
-import sys
+import pymysql, requests, os, sys, re
 
 app = Flask(__name__)
 app.secret_key = "hogehoge"
@@ -59,6 +56,16 @@ def login_check():
 def index():
     login_info = login_check()
     return render_template("index.html", login_info=login_info)
+
+#管理者データベース操作画面
+@app.route('/admin', methods = ["GET", "POST"])
+def admin_ctrl():
+    login_info = login_check()
+    db_result = db_access("", "show databases;")
+    db_list = []
+    for i in db_result:
+        db_list.append(i["Database"])
+    return render_template("admin_select.html", db_list=db_list, login_info=login_info)
 
 #Ajaxでデータベース選択時のテーブル一覧送信
 @app.route('/ajax_db', methods=["GET", "POST"])
@@ -116,20 +123,45 @@ def register_check():
     mail_str = request.form['mail_str']
     user_str = request.form['user_str']
     passwd_str = request.form['password_str']
-    sql_query = "select count(*) from member where user ='%s' or mail = '%s';"%(user_str, mail_str)
-    print("クエリは"+sql_query)
-    db_result = db_access(str('final_research'), sql_query)
-    print("これが問い合わせ結果→"+str(db_result[0]['count(*)']))
-    if db_result[0]['count(*)'] != 0:
-        Already = "そのユーザーは既に存在しています"
-    else:
-        Already = "登録しました！"
-        #dbにinsert
-        print("dbにinsert")
-        sql_query = "insert into member (mail, user, password) values('%s', '%s', '%s');"%(mail_str, user_str, passwd_str)
-        db_insert("final_research", sql_query)
 
-    return render_template("register.html", login_info=login_info, Already=Already, MailAddress=mail_str, UserName=user_str, PassWord=passwd_str)
+    Already = ""
+    validation_message = mail_validation(mail_str)
+
+    if validation_message == True:
+        sql_query = "select count(*) from member where user ='%s' or mail = '%s';"%(user_str, mail_str)
+        print("クエリは"+sql_query)
+        db_result = db_access(str('final_research'), sql_query)
+        print("これが問い合わせ結果→"+str(db_result[0]['count(*)']))
+        if db_result[0]['count(*)'] != 0:
+            Already = "そのユーザーは既に存在しています"
+        else:
+            Already = "登録しました！"
+            #dbにinsert
+            print("dbにinsert")
+            sql_query = "insert into member (mail, user, password) values('%s', '%s', '%s');"%(mail_str, user_str, passwd_str)
+            db_insert("final_research", sql_query)
+        validation_error = ""
+    else:
+        validation_error = "メールアドレスの形式ではありません"
+    
+
+    return render_template("register.html", login_info=login_info\
+        , Already=Already, MailAddress=mail_str, UserName=user_str, PassWord=passwd_str\
+            , validation_message=validation_error)
+
+#バリデーション判定用の関数(入力したメールアドレス)
+def mail_validation(mail_address):
+    #メールアドレスの形式である
+    error_message = True
+    result = re.fullmatch(r'^[0-9a-zA-Z_\.]+@([0-9a-zA-Z])+\.([0-9a-zA-Z\.])+([0-9a-zA-Z])+$', mail_address)
+
+    if result is None:
+        #メールアドレスの形式になっていない
+        error_message = False
+    elif len(re.sub(r"\.+", ".",mail_address)) != len(mail_address):
+        # . が連続されているため、メールアドレスの形式になっていない
+        error_message = False
+    return error_message
 
 #ログインページ
 @app.route('/login', methods = ["GET", "POST"])
@@ -146,9 +178,10 @@ def home():
     db_result = db_access(str('final_research'), sql_query)
     print(db_result[0]['count(*)'])
     
+
     if db_result[0]["count(*)"] == 1:
         session['logged_in'] = True
-        session['user_name'] = user_str+"はログイン中"
+        session['user_name'] = user_str
     else:
         session['logged_in'] = False
     return login_form()
@@ -156,15 +189,62 @@ def home():
 #検索ページ
 @app.route('/search', methods = ["GET", "POST"])
 def search_form():
+#----------------------------検索条件をもとに検索結果を絞る(where?)----------------------------
+    #where データ名="値"の値は空でも文字列でも整数型でも""で囲めばいける？ 2020-12-18
+    #とりあえずはできたけど、検索後に入力値消えるから会員登録を参考に完成させろよ
     login_info = login_check()
-    db_result = db_access("", "show databases;")
-    db_list = []
-    for i in db_result:
-        db_list.append(i["Database"])
-    return render_template("search.html", db_list=db_list, login_info=login_info)
+    select_school_type = ""
+    select_subjects = ""
+    select_grade = ""
+    subject_title = ""
+    done_year = ""
+    if request.method == "POST":
+        select_school_type = request.form['select_school_type']
+        select_subjects = request.form['select_subjects']
+        select_grade = request.form['select_grade']
+        subject_title = request.form['subject_title']
+        done_year = request.form['done_year']
+    print("これが検索条件"+select_school_type+", "+select_subjects+", "+select_grade+", "+subject_title+", "+done_year)
+#-----------------------uploads_listの教材情報を表示するための準備-----------------------
+    empty_box = select_school_type+select_subjects+select_grade+subject_title+done_year
+    if empty_box == "":
+        sql_query ="select school_type, subjects, grade, title, file_name, user_id, year from uploads_list;"
+    else:
+        sql_query = "select school_type, subjects, grade, title, file_name, user_id, year from uploads_list\
+            where school_type='%s' or subjects='%s' or grade='%s' or title='%s' or year='%s'"%(select_school_type, select_subjects, select_grade, subject_title, done_year)
+    result_uploads_list = db_access("final_research", sql_query)
+    print(result_uploads_list)
+    #校種リストを準備
+    school_type_list = db_access("final_research", "select * from school_type;")
+    #強化領域等リストを準備
+    subjects_list = db_access("final_research", "select * from subjects;")
+    #学年リストを準備
+    grade_list = db_access("final_research", "select * from grade;")
+    #ユーザー名を準備（上みたいに全部をリスト準備するとバカなので、必要なものだけを集める）
+    maker_list = db_access("final_research", "select distinct user_id from uploads_list;")
+    print("これが重複を除外した作成者番号"+str(maker_list))
+    #重複除外作成者番号をjsonから値だけのリスト化
+    user_id_list=[]
+    for i in maker_list:
+        user_id_list.append(i['user_id'])
+    print(user_id_list)
+    #keyをユーザーid、valueをユーザー名のjsonを準備
+    id_name_json = [{}]
+    for i in user_id_list:
+        user_query = "select user from member where id = %s"%(i)
+        print(db_access("final_research", user_query)[0]['user'])
+        id_name_json[0].update([(i, (db_access("final_research", user_query)[0]['user']))])
+    print("これがkey=id:value=username→"+str(id_name_json))
+#--------------------------------------ここまで---------------------------------------
+    return render_template("search.html"\
+        , login_info=login_info, result_uploads_list=result_uploads_list\
+            , school_type_list=school_type_list, subjects_list=subjects_list, grade_list=grade_list, id_name_json=id_name_json)
 
+#ファイルのアップロード先のディレクトリ
 UPLOAD_FOLDER = './static/uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+#アップロードされるファイルの拡張子の制限
+ALLOWED_EXTENSIONS = set(['pdf', 'docx', 'pptx', 'doc', 'ppt', 'txt'])
+#?????
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 #共有ページ
@@ -172,23 +252,57 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def pptx_upload():
     login_info = login_check()
     try:
+        #もしログインしてるなら、共有作業
         if session['logged_in'] == True:
-            #↓アップロード試作
+            #↓リクエストがポスト可動科の判別
             if request.method == 'POST':
+                #ファイルがなかった場合の処理
                 if 'file' not in request.files:
                     print('ファイルがありません')
                     return redirect(request.url)
+                #データの取り出し
                 file = request.files['file']
+                #ファイル名が無かった時の処理
                 if file.filename == '':
                     print('ファイルがありません')
                     return redirect(request.url)
+                #ファイルのチェック
                 if file and allwed_file(file.filename):
+                    #危険な文字を削除(サニタイズ処理？)→ここで絶対パスとか消してるの？？？どうなの？？？
                     filename = secure_filename(file.filename)
+                    #ファイルの保存
                     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    #-------------------------↓データベースのuploads_listに登録する-------------------------
+                    select_school_type = request.form['select_school_type']
+                    select_subjects = request.form['select_subjects']
+                    select_grade = request.form['select_grade']
+                    subject_title = request.form['subject_title']
+                    done_year = request.form['done_year']
+                    #↓session['user_name']の中のユーザーネームでidを問い合わせる
+                    user_id_query = "select id from member where user='%s';"%(session['user_name'])
+                    print(user_id_query)
+                    #↓データ名がidしかない表から値を取り出す
+                    user_id = db_access("final_research", user_id_query)[0]['id']
+                    print(user_id)
+                    #↓insertのためのカラム
+                    insert_column = "file_name, user_id, school_type, subjects, grade, title, year"
+                    #↓まずはただのinsertのvaluesのリスト
+                    value_list = [filename, int(user_id), int(select_school_type), int(select_subjects), int(select_grade), subject_title, done_year]
+                    #↓上のvalue_listの[]がうざいので除去
+                    insert_value = ", ".join(repr(e) for e in value_list)
+                    print(", ".join(repr(e) for e in value_list))
+                    #↓uploads_listテーブルにデータを登録するクエリ
+                    sql_query = "insert into uploads_list (%s) values(%s)"%(insert_column, insert_value)
+                    print(sql_query)
+                    #↓先に用意したクエリをもとにinsert
+                    db_insert("final_research", sql_query)
+                    #--------------------------------------↑ここまで--------------------------------------
+                #アップロード後のページに転送
                 return redirect(url_for('uploaded_file', filename=filename))
-            #↑ここまで
-            return render_template("upload.html", login_info=login_info)
+            #アップロード画面のリターン？
+            return render_template("upload.html", login_info=login_info, upload_status="ファイルを選択してください")
         else:
+            #ログインしてない場合はまずログインさせる
             return render_template("login.html", Already="共有するにはログインしてください", login_info=login_info)
     except:
         #一度もログイン作業を行なっていないとエラーになるからそのときもログインを促す
@@ -196,10 +310,15 @@ def pptx_upload():
     #return render_template("upload.html", login_info=login_info)
 
 @app.route('/uploads/<filename>')
+#アップロード後に転送されるページ
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    login_info = login_check()
+    #return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return render_template("upload.html", login_info=login_info, upload_status="アップロードしました！")
 
 def allwed_file(filename):
+    #.があるかどうかのチェックと、拡張子の確認
+    #okなら1, だめなら0
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == '__main__':
